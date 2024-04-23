1e0eee651cadd6fce10ab85edebabafb6eea4f80
Stacy <146508027+TyuninaA@users.noreply.github.com>
2024-04-23 16:01:46 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,73 @@
 import Head from 'next/head'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2'
 import fetch from 'node-fetch'
 import Papa from 'papaparse'
-import Chart from 'chart.js/auto';
-
+import Chart from 'chart.js/auto'
 export default function Home({ data }) {
-  const [selectedRegion, setSelectedRegion] = useState(null);
+  const [selectedRegion, setSelectedRegion] = useState("");
   const [showTotal, setShowTotal] = useState(true);
   const [showMales, setShowMales] = useState(true);
   const [showFemales, setShowFemales] = useState(true);
+  const [chartData, setChartData] = useState(null);
 
   const regionLabels = data.populations.data.slice(1).map(item => item[0]);
   const totalPopulations = data.populations.data.slice(1).map(item => item[1]);
   const males = data.populations.data.slice(1).map(item => item[2]);
   const females = data.populations.data.slice(1).map(item => item[3]);
+
+  useEffect(() => {
+    if (selectedRegion === "") {
+      setChartData({
+        labels: regionLabels,
+        datasets: [
+          {
+            label: 'Всего',
+            data: totalPopulations,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 1
+          },
+          {
+            label: 'Мужчины',
+            data: males,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgb(54, 162, 235)',
+            borderWidth: 1
+          },
+          {
+            label: 'Женщины',
+            data: females,
+            backgroundColor: 'rgba(255, 205, 86, 0.2)',
+            borderColor: 'rgb(255, 205, 86)',
+            borderWidth: 1
+          }
+        ]
+      });
+    } else {
+      const selectedIndex = regionLabels.indexOf(selectedRegion);
+      setChartData({
+        labels: ['Всего', 'Мужчины', 'Женщины'],
+        datasets: [
+          {
+            label: selectedRegion,
+            data: [totalPopulations[selectedIndex], males[selectedIndex], females[selectedIndex]],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 205, 86, 0.2)'
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)'
+            ],
+            borderWidth: 1
+          }
+        ]
+      });
+    }
+  }, [selectedRegion]);
 
   const handleChangeRegion = (event) => {
     setSelectedRegion(event.target.value);
@@ -31,26 +84,6 @@ export default function Home({ data }) {
   const handleChangeShowFemales = () => {
     setShowFemales(!showFemales);
   };
-
-  let filteredRegionIndex = null;
-  if (selectedRegion) {
-    filteredRegionIndex = regionLabels.findIndex(label => label === selectedRegion);
-  }
-
-  const selectedData = [];
-  const selectedLabels = [];
-  if (showTotal) {
-    selectedLabels.push('Всего');
-    selectedData.push(totalPopulations[filteredRegionIndex]);
-  }
-  if (showMales) {
-    selectedLabels.push('Мужчины');
-    selectedData.push(males[filteredRegionIndex]);
-  }
-  if (showFemales) {
-    selectedLabels.push('Женщины');
-    selectedData.push(females[filteredRegionIndex]);
-  }
 
   return (
     <>
@@ -99,69 +132,63 @@ export default function Home({ data }) {
             <label htmlFor="showFemales" className="text-gray-700">Показать Женщин</label>
           </div>
         </div>
-        {selectedRegion && (
-          <div className="flex flex-col items-center justify-center">
-            <div className="w-1/2 mb-4">
-              <Bar
-                options={{
-                  plugins: {
-                    title: {
-                      display: true,
-                      text: selectedRegion,
-                      font: {
-                        size: 14
-                      }
-                    },
-                    legend: {
-                      display: true,
-                      position: 'bottom'
+        {chartData && (
+          <div className="w-full mx-auto mb-4">
+            <Bar
+              options={{
+                plugins: {
+                  title: {
+                    display: true,
+                    text: 'Население городов Казахстана по регионам',
+                    font: {
+                      size: 16
                     }
                   },
-                  responsive: true
-                }}
-                data={{
-                  labels: selectedLabels,
-                  datasets: [
-                    {
-                      label: 'Население',
-                      data: selectedData,
-                      backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 205, 86, 0.2)'
-                      ],
-                      borderColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)',
-                        'rgb()'
-                      ],
-                      borderWidth: 1
-                    }
-                  ]
-                }}
-              />
-            </div>
-            <div className="w-1/2">
-              <h4 className="text-xl font-semibold mb-2">Данные по выбранному региону:</h4>
-              <table className="table-auto border-collapse border border-gray-400">
-                <thead>
-                  <tr>
-                    <th className="border border-gray-400 px-4 py-2">Показатель</th>
-                    <th className="border border-gray-400 px-4 py-2">Значение</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {selectedLabels.map((label, index) => (
-                    <tr key={label}>
-                      <td className="border border-gray-400 px-4 py-2">{label}</td>
-                      <td className="border border-gray-400 px-4 py-2">{selectedData[index]}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                  legend: {
+                    display: true,
+                    position: 'bottom'
+                  }
+                },
+                responsive: true,
+                maintainAspectRatio: false
+              }}
+              data={chartData}
+              height={400}
+
+            />
           </div>
         )}
+        {selectedRegion && (
+          <div className="w-full mx-auto mb-4">
+            <h4 className="text-xl font-semibold mb-2 text-center">Данные по выбранному региону: {selectedRegion}</h4>
+            <table className="table-auto border-collapse border border-gray-400 mx-auto">
+              <thead>
+                <tr>
+                  <th className="border border-gray-400 px-4 py-2">Показатель</th>
+                  <th className="border border-gray-400 px-4 py-2">Значение</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border border-gray-400 px-4 py-2">Всего</td>
+                  <td className="border border-gray-400 px-4 py-2">{totalPopulations[regionLabels.indexOf(selectedRegion)]}</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-400 px-4 py-2">Мужчины</td>
+                  <td className="border border-gray-400 px-4 py-2">{males[regionLabels.indexOf(selectedRegion)]}</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-400 px-4 py-2">Женщины</td>
+                  <td className="border border-gray-400 px-4 py-2">{females[regionLabels.indexOf(selectedRegion)]}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        )}
+        <div className="flex justify-end mt-4 mx-auto">
+          <a href="https://github.com/open-data-kazakhstan/city-population.git" className="text-blue-500 mr-4" target="_blank" rel="noopener noreferrer">Ссылка на GitHub</a>
+          <a href="https://github.com/open-data-kazakhstan/city-population/blob/1050c4217988a6eb1526198061b36b942cb3997b/data/city_population.csv" className="bg-blue-500 text-white px-4 py-2 rounded-lg" download>Скачать датасет</a>
+        </div>
       </main>
     </>
   )
